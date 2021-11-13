53ea5f4337fe75ef726e58feeafa2470250af935
S0raWasTaken <anosensedroi@gmail.com>
2021-11-13 06:42:38 -0500

    Modify password generation method

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use clap::{App, Arg};
-use passwords::PasswordGenerator;
+
+use crate::password_generator::generate_pw;
+
+mod password_generator;
 
 fn main() -> Result<(), String> {
-    let mut pass_gen = PasswordGenerator::new();
-    pass_gen.exclude_similar_characters = true;
-
     let matches = App::new("Rugen")
         .version(format!("v{}", env!("CARGO_PKG_VERSION")).as_str())
         .author("by S0raWasTaken")
@@ -32,33 +32,9 @@ fn main() -> Result<(), String> {
         .map_err(|_| String::from("That size was not a number"))?;
 
     if let Some(t) = matches.value_of("type") {
-        match t.to_ascii_lowercase().as_str() {
-            "lowest" => {
-                pass_gen.numbers = false;
-                pass_gen.length = size;
-                println!("{}", pass_gen.generate_one()?);
-                Ok(())
-            }
-            "low" => {
-                pass_gen.length = size;
-                println!("{}", pass_gen.generate_one()?);
-                Ok(())
-            }
-            "medium" => {
-                pass_gen.uppercase_letters = true;
-                pass_gen.length = size;
-                println!("{}", pass_gen.generate_one()?);
-                Ok(())
-            }
-            "high" => {
-                pass_gen.uppercase_letters = true;
-                pass_gen.symbols = true;
-                pass_gen.length = size;
-                println!("{}", pass_gen.generate_one()?);
-                Ok(())
-            }
-            _ => Err(String::from("Password type unknown")),
-        }
+        generate_pw(t, size)?;
+        //println!("{}", pw);
+        Ok(())
     } else {
         Err(String::from("How the hell did we get here?"))
     }
